761058759d715840433cf01b4857ddc240f51408
iThanathat <ithanathat.brain@gmail.com>
2023-03-08 02:58:29 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#000000",
+          secondary: "#34A853",
+        },
+      },
+    ],
+  },
+};
