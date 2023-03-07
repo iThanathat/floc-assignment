dfa977b660ef6c233ee56c41de1290628a7b31a3
iThanathat <ithanathat.brain@gmail.com>
2023-03-08 03:01:28 +0700

    Wrapped application with AppLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/global.css'
-import type { AppProps } from 'next/app'
+import AppLayout from "@/components/Layouts/app";
+import "@/styles/global.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppLayout className={poppins.className}>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 }
