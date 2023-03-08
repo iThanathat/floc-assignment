73dbee361612984d932dff2ecf6664ff3114d28a
iThanathat <ithanathat.brain@gmail.com>
2023-03-09 02:07:00 +0700

    Added shadow and text color

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const NavBar = () => {
   return (
-    <div className="w-full flex justify-between p-4">
+    <div className="relative w-full flex justify-between p-4 shadow-lg z-[9999]">
       <div className="w-1/2">
         <Image src="/images/floc-logo.svg" alt="floc" width={50} height={50} />
       </div>
-      <div className="flex w-1/2 justify-evenly">
+      <div className="flex w-1/2 justify-evenly text-primary">
         <p>Creatives</p>
         <p>Package</p>
         <p>Jobs</p>
