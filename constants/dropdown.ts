e658800960f97a553b41c3c2bf531cd1d20d2d34
iThanathat <ithanathat.brain@gmail.com>
2023-03-09 02:07:48 +0700

    Added constant file for dropdown option

---
+++ constants/dropdown.ts
@@ -0,0 +1,77 @@
+import { OptionsType } from "@/components/Select/types";
+
+export const CONTENT_TYPE_OPTIONS: OptionsType = [
+  {
+    value: "content type 1",
+    label: "content type 1",
+  },
+  {
+    value: "content type 2",
+    label: "content type 2",
+  },
+  {
+    value: "content type 3",
+    label: "content type 3",
+  },
+  {
+    value: "content type 4",
+    label: "content type 4",
+  },
+];
+
+export const OBJECTIVE_OPTIONS: OptionsType = [
+  {
+    value: "objective 1",
+    label: "objective 1",
+  },
+  {
+    value: "objective 2",
+    label: "objective 2",
+  },
+  {
+    value: "objective 3",
+    label: "objective 3",
+  },
+  {
+    value: "objective 4",
+    label: "objective 4",
+  },
+];
+
+export const VALUE_OPTIONS: OptionsType = [
+  {
+    value: "value 1",
+    label: "value 1",
+  },
+  {
+    value: "value 2",
+    label: "value 2",
+  },
+  {
+    value: "value 3",
+    label: "value 3",
+  },
+  {
+    value: "value 4",
+    label: "value 4",
+  },
+];
+
+export const HIRING_MODEL_OPTIONS: OptionsType = [
+  {
+    value: "hiring model 1",
+    label: "hiring model 1",
+  },
+  {
+    value: "hiring model 2",
+    label: "hiring model 2",
+  },
+  {
+    value: "hiring model 3",
+    label: "hiring model 3",
+  },
+  {
+    value: "hiring model 4",
+    label: "hiring model 4",
+  },
+];
