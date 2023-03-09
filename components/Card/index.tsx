1625a05031618c5f45846e450d389858cb9e2e74
iThanathat <ithanathat.brain@gmail.com>
2023-03-10 03:10:36 +0700

    Added className props

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
 import { CardProps } from "./types";
 import styles from "./index.module.css";
 
-const Card: React.FC<CardProps> = ({ children }) => {
-  return <div className={styles.card}>{children}</div>;
+const Card: React.FC<CardProps> = ({ children, className }) => {
+  return (
+    <div className={`${className ? className : ""} ${styles.card}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
