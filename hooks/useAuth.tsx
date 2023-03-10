a5c7eaa0fbbcd97dc4198f59101946520849e6c8
iThanathat <ithanathat.brain@gmail.com>
2023-03-10 13:37:48 +0700

    Moved login function into hook useAuth

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -3,6 +3,29 @@ import { useEffect, useState } from "react";
 const useAuth = () => {
   const [isLogin, setIsLogin] = useState<boolean>(false);
 
+  const login = async (username: string, password: string): Promise<any> => {
+    try {
+      const response = await fetch(`${window.location.origin}/api/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      const result = await response.json();
+      if (result.token) {
+        setIsLogin(true);
+        localStorage.setItem("token", result.token);
+      } else {
+        setIsLogin(false);
+      }
+      return result;
+    } catch (error) {
+      throw new Error(error as string);
+    }
+  };
+
   useEffect(() => {
     const token = localStorage.getItem("token");
 
@@ -14,6 +37,7 @@ const useAuth = () => {
   return {
     isLogin,
     setIsLogin,
+    login,
   };
 };
 
